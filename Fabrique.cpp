c23bff9554359155033f983eec6bb3232ac7d2e3
Hugo Moynac <hugo.moynac@gmail.com>
2016-02-02 13:35:59 +0100

    Fabrique.cpp #1
    Tout est à faire !

---
+++ Fabrique.cpp
@@ -0,0 +1,57 @@
+#include "Fabrique.h"
+
+Fabrique::Fabrique()
+{
+    //ctor
+}
+
+Fabrique::~Fabrique()
+{
+    //dtor
+}
+void Fabrique::creerR(string nom, int x1,int y1,int x2, int y2)
+{
+
+
+}
+void Fabrique::creerS(string nom, int x1,int y1,int x2, int y2)
+{
+
+
+}
+void Fabrique::creerPC(string nom, int taille ,int c[])
+{
+
+
+}
+void Fabrique::unir(string nom, int taille, string formes[])
+{
+
+
+}
+void Fabrique::intersection(string nom, int taille, string formes[])
+{
+
+
+}
+void Fabrique::bouger(string nom, int dx,int dy)
+{
+
+
+}
+bool Fabrique::appartenance(string nom, int x, int y)
+{
+
+    return true;
+}
+void Fabrique::lister()
+{
+
+
+}
+
+void Fabrique::nettoyer()
+{
+
+}
+
